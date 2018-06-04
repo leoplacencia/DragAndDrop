b49209b4b29a4beb2e8a2418df8cda6fe4e6ce69
Leo Placencia <lplacencia@moldeable.com>
2018-06-04 11:33:36 -0400

    Cargar fotos correcto, ver mostrar fotos.

--- src/app/components/fotos/fotos.component.ts
+++ src/app/components/fotos/fotos.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+
+export interface Item { nombre: string; url: string; }
+
 @Component({
   selector: 'app-fotos',
   templateUrl: './fotos.component.html',
   styles: []
 })
 export class FotosComponent implements OnInit {
+  private itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) {
+    this.itemsCollection = afs.collection<Item>('img');
+    this.items = this.itemsCollection.valueChanges();
+  }
 
   ngOnInit() {
   }
--- src/app/services/carga-imagenes.service.ts
+++ src/app/services/carga-imagenes.service.ts
@@ -10,9 +10,33 @@ export class CargaImagenesService {
   constructor( private db: AngularFirestore) { }
 
   cargarImagenesFirebase( imagenes: FileItem[]) {
-    console.log(imagenes);
+    const storageRef = firebase.storage().ref();
+    for ( const item of imagenes ) {
+      item.estaSubiendo = true;
+      if (item.progreso >= 100) {
+        continue;
+      }
+      const uploadTask: firebase.storage.UploadTask = storageRef.child(`${ this.CARPETA_IMAGENES }/${ item.nombreArchivo }`)
+      .put( item.archivo );
+
+      uploadTask.on( firebase.storage.TaskEvent.STATE_CHANGED,
+        (snapshot: firebase.storage.UploadTaskSnapshot) => item.progreso =
+        ( snapshot.bytesTransferred / snapshot.totalBytes ) * 100,
+        ( error ) => console.error('Error al subir', error),
+        () => {
+          console.log('imagen cargada correctamente.');
+          item.url = uploadTask.snapshot.downloadURL;
+          item.estaSubiendo = false;
+          this.guardarImagen({
+            nombre: item.nombreArchivo,
+            url: item.url
+          });
+        });
+
+    }
+
   }
-  private guardarImage ( imagen: { nombre: string, url: string }) {
+  private guardarImagen ( imagen: { nombre: string, url: string }) {
     this.db.collection(`/${ this.CARPETA_IMAGENES }`)
       .add( imagen );
   }
